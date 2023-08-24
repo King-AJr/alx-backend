569341a0cbdf0e499e5e2cff0ddea8492ba9411d
King-AJr <talk2king.aj@gmail.com>
2023-08-24 08:15:51 +0100

    follow standard syntax

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,6 +14,7 @@ class LRUCache(BaseCaching):
     class to implement LRU caching
     algorithm
     """
+
     cache = OrderedDict()
 
     def __init__(self):
@@ -30,7 +31,8 @@ class LRUCache(BaseCaching):
             self.cache_data.pop(key)
         elif len(self.cache_data) == self.MAX_ITEMS:
             # If cache is full, remove the first (least recently used) item
-            discarded_key, discarded_value = self.cache_data.popitem(last=False)
+            discarded_key, discarded_value = \
+                self.cache_data.popitem(last=False)
             print("DISCARD: {}".format(discarded_key))
         self.cache_data[key] = item
 
@@ -38,8 +40,9 @@ class LRUCache(BaseCaching):
         """Get an item by key"""
         if key in self.cache_data:
             value = self.cache_data.pop(key)
-            self.cache_data[key] = value  # Move the accessed item to the beginning
+            # Move the accessed item to the beginning
+            self.cache_data[key] = value
             self.cache_data = self.cache_data
             return key
-        
+
         return None
