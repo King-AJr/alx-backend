209d9450be528b46ed754a480e16ffa470f6c277
King-AJr <talk2king.aj@gmail.com>
2023-09-26 15:52:12 +0100

    create a queue process that will listen to new jobs on push_notification_code

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,10 +1,10 @@
 import { createQueue } from "kue";
 
-const queue = createQueue({name: 'push_notification_code'});
+const queue = createQueue();
 
 const job_data = queue.create('push_notification_code',{
-  phoneNumber: '+2347056839557',
-  message: 'this is my whatsapp contact',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 });
 
 //created without error
